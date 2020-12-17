6508f0e430e09b69ee87bd9c25f11acad5620913
FanChoyYen <73092791+FanChoyYen@users.noreply.github.com>
2020-12-17 21:05:22 +0800

    1

---
+++ src/app/models/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    username: string;
+    password: string;
+    password2: string;
+    name: string;
+    ic: number;
+    address1: string;
+    address2: string;
+    postcode: number;
+    state: string;
+    email: string;
+    phone: number;
+    resident: string;
+}
